2e0ecbb177986a249f886cd32aba1118bfe7c712
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-26 16:51:45 +0700

    understand tuples in scala

---
+++ Collections/tuples.scala
@@ -0,0 +1,12 @@
+case class Person(name: String)
+
+@main def main =
+  val t = (7, "eleven", Person("Mike"))
+
+  // Tuple extract
+  val (num, store, person) = t
+
+  println(s"First value ${t(0)}")
+  println(s"Second value ${t(1)}")
+  println(s"Third value ${t(2)}")
+  println(s"$num$store ${person.name}")
