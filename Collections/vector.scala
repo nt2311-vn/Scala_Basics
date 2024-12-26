c39df73f244edc3eba7a212d3bb4bc72c9a95395
nt2311 <nguyentoan231196@gmail.com>
2024-12-26 23:44:46 +0700

    understand vector and prepend and append method using operator

--- Collections/vector.scala
+++ Collections/vector.scala
@@ -5,5 +5,13 @@
   case class Person(name: String)
   val people = Vector(Person("Bert"), Person("Ernie"), Person("Grover"))
 
+  // Append and prepend new element vector create new sequence
+  val a = Vector(1, 2, 3)
+  val b = a :+ 4 // append element to vector
+  val c = a ++ Vector(4, 5) // append element of vector to vector
+
   println(nums)
   println(people)
+  println(a)
+  println(b)
+  println(c)
