909396269dd4fc20cbfe141fe8af609d6acf7c3c
nt2311 <nguyentoan231196@gmail.com>
2024-12-27 00:04:20 +0700

    how to prepend element to vector and prepend all vector to vector

--- Collections/vector.scala
+++ Collections/vector.scala
@@ -9,9 +9,13 @@
   val a = Vector(1, 2, 3)
   val b = a :+ 4 // append element to vector
   val c = a ++ Vector(4, 5) // append element of vector to vector
+  val d = 0 +: a // prepend element to vector
+  val e = Vector(-1, 0) ++: a // alias for prepend all
 
   println(nums)
   println(people)
   println(a)
   println(b)
   println(c)
+  println(d)
+  println(e)
