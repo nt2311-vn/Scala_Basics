4d63157d6be05ce496b3ca3e2a3f0c86d1320e2e
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-26 11:16:09 +0700

    concrete not allow modify

--- DomainModeling/class.scala
+++ DomainModeling/class.scala
@@ -1,8 +1,7 @@
 @main def main =
   val p = Person("Toan", "Nguyen")
   println(p.firstName)
-  p.lastName = "Legend"
   p.printFullName()
 
-class Person(var firstName: String, var lastName: String):
+class Person(val firstName: String, val lastName: String):
   def printFullName() = println(s"$firstName $lastName")
