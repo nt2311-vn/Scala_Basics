fc65b925ea5989fa01f64ced69579b39e4b4b5b7
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-27 09:27:44 +0700

    methods of array buffer and syntax to modify element

--- Collections/arraybuffer.scala
+++ Collections/arraybuffer.scala
@@ -2,18 +2,35 @@ import scala.collection.mutable.ArrayBuffer
 
 @main def arraybuffer =
 
-  // create empty array buffer
+  /** create empty array buffer */
+
   case class Person(name: String)
   var strings = ArrayBuffer[String]()
   var ints = ArrayBuffer[Int](20)
   var people = ArrayBuffer[Person]()
 
-  // create with initial elements
+  /** create with initial elements */
+
   val nums = ArrayBuffer(1, 2, 3)
   val guestList = ArrayBuffer(Person("Bert"), Person("Ernie"), Person("Grover"))
+
+  /** methods */
+
+  // Add an element to array buffer
+  nums += 4
+
+  // array buffer is mutable so add or remove direct modify the original array (modify the reference storage)
+
+  nums ++= List(5, 6)
+  nums.prependAll(List(-1, 0))
+  nums.prepend(-2)
+
+  // update element in array buffer
+  nums(0) = -3
 
   println(strings)
   println(ints)
   println(people)
   println(nums)
   println(guestList)
+  println(nums)
