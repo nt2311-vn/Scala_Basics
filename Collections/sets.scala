6d2de26d3473d5c396f5c462691d1e81c2450bf7
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-27 10:45:11 +0700

    discover set and init empty and pre-value set

---
+++ Collections/sets.scala
@@ -0,0 +1,11 @@
+@main def set =
+  /** Init new empty and static elements */
+  val emptyNums = Set[Int]()
+  val emptyLetters = Set[Char]()
+  val nums = Set(1, 2, 3, 4)
+  val letters = Set('a', 'b', 'c', 'd')
+
+  println(emptyNums)
+  println(emptyLetters)
+  println(nums)
+  println(letters)
