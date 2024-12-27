0cfaab5eb6667c3a0a04b8401eb0dc167763a264
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-27 11:36:12 +0700

    order become arbitrary add a set or seq to set its become hash set

--- Collections/sets.scala
+++ Collections/sets.scala
@@ -5,7 +5,16 @@
   val nums = Set(1, 2, 3, 4)
   val letters = Set('a', 'b', 'c', 'd')
 
+  /** Add element and set to set, reassign new variable if set is imutable, set
+    * in scala can be both imutable and mutable
+    */
+
+  val a = Set(1, 2)
+  var b = Set('a', 'b', 'c')
+  val c = a + 3 ++ Seq(4, 5)
+
   println(emptyNums)
   println(emptyLetters)
   println(nums)
   println(letters)
+  println(c)
