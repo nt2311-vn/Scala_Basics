a453dc23b9d1435ecd988aa809725cfcfe9b022e
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-27 10:00:29 +0700

    add or remove element using method and sign operators

--- Collections/maps.scala
+++ Collections/maps.scala
@@ -18,10 +18,21 @@
   val sg = states("SG")
 
   // using get or else or with default value when init maps to avoid crash when access not exisit key
+
   val dn = states.getOrElse("DN", "key not exist")
+
+  /* Add and remove element from Map */
+  // Assign to new variable since the default Map is imutable every update remove will create new map
+  var newStates = states + ("DN" -> "Da Nang")
+  newStates = newStates + ("DN" -> "Dong Nai")
+  println(s"DN now is ${newStates.getOrElse("DN", "key not exist")}")
+
+  newStates = newStates ++ Seq("DN" -> "Da Nang", "HA" -> "Hoi An")
+  newStates = newStates - "HA"
 
   println(states.size)
   println(emptyMap.size)
   println(qn)
   println(sg)
   println(dn)
+  println(newStates)
