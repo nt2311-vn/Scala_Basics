4c7d411ce7b9d8f52c34f82ff4521023d1131f6d
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-30 10:04:28 +0700

    partial function use as argument in parameter

--- Functions/partialFunction.scala
+++ Functions/partialFunction.scala
@@ -5,3 +5,6 @@
 
   val checkInts = List(3, 4, 5)
   checkInts.foreach(i => println(doubledOdds.isDefinedAt(i)))
+
+  val res = List(1, 2, 3).collect({ case i if i % 2 == 1 => i * 2 })
+  println(res)
