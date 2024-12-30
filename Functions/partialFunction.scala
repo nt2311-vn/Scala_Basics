bbdb8cb6d1111a35917fa93c01449790f2db5e57
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-30 10:28:36 +0700

    understand partial functions

--- Functions/partialFunction.scala
+++ Functions/partialFunction.scala
@@ -3,8 +3,16 @@
     case i if i % 2 == 1 => i * 2
   }
 
+  val incrementEvents: PartialFunction[Int, Int] = {
+    case i if i % 2 == 0 => i + 1
+  }
+
   val checkInts = List(3, 4, 5)
   checkInts.foreach(i => println(doubledOdds.isDefinedAt(i)))
 
   val res = List(1, 2, 3).collect({ case i if i % 2 == 1 => i * 2 })
+  val res2 = List(1, 2, 3).collect(doubledOdds.orElse(incrementEvents))
+
   println(res)
+  println(doubledOdds.applyOrElse(4, _ + 1))
+  println(res2)
