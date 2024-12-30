f2c49f941f45562fe0d2c4e8320125a3fd7f8d7f
nt2311 <nguyentoan231196@gmail.com>
2024-12-30 22:44:46 +0700

    imutable with class using case class

--- FP/immutable.scala
+++ FP/immutable.scala
@@ -3,3 +3,9 @@
   val b = a.filter(_.startsWith("j")).map(_.capitalize)
 
   println(b)
+
+  case class Person(fistName: String, lastName: String)
+  val reginald = Person("Reginald", "Dwight")
+  val elton = reginald.copy("Elton", "John")
+
+  println(s"${elton.fistName} ${elton.lastName}")
