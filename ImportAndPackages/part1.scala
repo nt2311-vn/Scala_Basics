5824477a9da10c417c79033e26706bc23331c3da
nt2311 <nguyentoan231196@gmail.com>
2024-12-30 20:52:19 +0700

    understand import rename and group import and hide import

--- ImportAndPackages/part1.scala
+++ ImportAndPackages/part1.scala
@@ -13,3 +13,23 @@ import users.* // import everything from users package
 import users.User // import only the User class
 import users.{User, UserPreferences} // import only the selected members
 import users.{UserPreferences as UPrefs} // rename an imported component
+
+import scala.concurrent.Future // Import a member from a pacakge
+import scala.concurrent.Promise
+import scala.concurrent.blocking
+
+import scala.concurrent.{
+  Future,
+  Promise,
+  blocking
+} // or using group syntax of selected member
+
+import java.util.{List as JavaList} // import an rename member
+import java.util.{Date as JDate, HashMap as JHashMap, *}
+
+import java.util.{
+  Random as _,
+  *
+} // hide selected import and import all the other pacakges
+
+import java.util.{List as _, Map as _, Set as _, *} // hide multiple members
