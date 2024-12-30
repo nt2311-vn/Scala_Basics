9ce204a894d23e9e530f6e509ce134ecadab9036
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-30 11:48:33 +0700

    multiple parameter mix function and x and y

--- Functions/hof.scala
+++ Functions/hof.scala
@@ -6,6 +6,15 @@
   def executeNTimes(f: () => Unit, n: Int): Unit =
     for i <- 1 to n do f()
 
+  def executeAndPrint(f: (Int, Int) => Int, i: Int, j: Int): Unit = println(
+    f(i, j)
+  )
+
+  def add(x: Int, y: Int): Int = x + y
+  def multiply(x: Int, y: Int): Int = x * y
+
   sayHello(helloToan)
   sayHello(bonjourToan)
   executeNTimes(helloToan, 5)
+  executeAndPrint(add, 2, 3)
+  executeAndPrint(multiply, 4, 5)
