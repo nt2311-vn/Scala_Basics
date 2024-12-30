99c8c7252d2b0ab81ae41ba161f9256a3b5fe168
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-30 11:28:09 +0700

    high order function with multiple params

--- Functions/hof.scala
+++ Functions/hof.scala
@@ -1,5 +1,11 @@
 @main def main =
   def sayHello(f: () => Unit): Unit = f()
   def helloToan(): Unit = println("Hello, Toan")
+  def bonjourToan(): Unit = println("Bonjour, Toan")
+
+  def executeNTimes(f: () => Unit, n: Int): Unit =
+    for i <- 1 to n do f()
 
   sayHello(helloToan)
+  sayHello(bonjourToan)
+  executeNTimes(helloToan, 5)
