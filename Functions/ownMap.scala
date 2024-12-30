c52197b5ca3ec35ed71bc34dcc5a0bc542f89842
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-30 15:31:48 +0700

    more generic on own map methof

--- Functions/ownMap.scala
+++ Functions/ownMap.scala
@@ -1,6 +1,15 @@
 @main def main =
-  def map[A](f: (Int) => A, xs: List[Int]) =
-    for x <- xs yield f(x)
+  // def map[A](f: (Int) => A, xs: List[Int]): List[A] =
+  //   for x <- xs yield f(x)
 
   def map[A, B](f: (B) => A, xs: List[B]): List[A] =
     for x <- xs yield f(x)
+
+  def double(i: Int): Int = i * 2
+  def strlen(s: String): Int = s.length
+
+  val x = map(double, List(1, 2, 3))
+  val y = map(strlen, List("a", "aab", "cc"))
+
+  println(x)
+  println(y)
