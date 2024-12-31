dcd96b9fec92022afeb5ad1122fe2d0d940d7f73
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-31 11:01:29 +0700

    using for loop to combine with mathemetic operation and apply function to multiple value

--- FP/errorhandling.scala
+++ FP/errorhandling.scala
@@ -16,3 +16,23 @@
   clearerMakeInt("bar") match
     case Some(i) => println(i)
     case None    => println("Cannot make int on provided value")
+
+  def consumeMsg(msg: Option[Int]) =
+    msg match
+      case Some(i) => i
+      case None    => None
+
+  val y = for
+    a <- clearerMakeInt("1")
+    b <- clearerMakeInt("2")
+    c <- clearerMakeInt("foo")
+  yield a + b + c
+
+  val z = for
+    a <- clearerMakeInt("1")
+    b <- clearerMakeInt("3")
+    c <- clearerMakeInt("5")
+  yield a + b + c
+
+  println(consumeMsg(z))
+  println(consumeMsg(y))
