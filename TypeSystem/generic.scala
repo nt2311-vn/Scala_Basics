b2b4e4cb743ed2a846d83ceaf51fbc2f2048ea88
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-31 14:34:04 +0700

    test generic on making a simple stack and utility method of it

---
+++ TypeSystem/generic.scala
@@ -0,0 +1,21 @@
+import java.{util => ju}
+@main def generic =
+  class Stack[A]:
+    private var elements: List[A] = Nil
+
+    def push(x: A): Unit =
+      elements = elements.prepended(x)
+
+    def peek: A = elements.head
+    def pop(): A =
+      val currentTop = peek
+      elements = elements.tail
+      currentTop
+
+  val stackA: Stack[Int] = Stack()
+  stackA.push(1)
+  stackA.push(5)
+  stackA.push(7)
+
+  println(stackA.peek)
+  println(stackA.pop())
