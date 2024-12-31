27b3cc9360f1f31bd558afda1a0d6567c151edd9
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-31 11:29:44 +0700

    use optional value Option[A] for declare a value either empty or has value

--- FP/null.scala
+++ FP/null.scala
@@ -1,12 +1,12 @@
 @main def nullval =
   class Address(
       var street1: String,
-      var street2: String,
+      var street2: Option[String],
       var city: String,
       var state: String,
       var zip: String
   )
 
-  val santa = Address("1 Main Street", null, "North Pole", "Alaska", "99705")
+  val santa = Address("1 Main Street", None, "North Pole", "Alaska", "99705")
 
   println(s"${santa.street2}")
