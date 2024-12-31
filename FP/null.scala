86eb39c2918678907fbcb9be7c67e444fa06bc08
nt2311-vn <nguyentoan231196@gmail.com>
2024-12-31 11:35:37 +0700

    null value but FP dont use null value

--- FP/null.scala
+++ FP/null.scala
@@ -8,5 +8,13 @@
   )
 
   val santa = Address("1 Main Street", None, "North Pole", "Alaska", "99705")
+  val britishPM = Address(
+    "10 Downing Street",
+    Some("Room 1"),
+    "London",
+    "Greater London",
+    "SW1A"
+  )
 
   println(s"${santa.street2}")
+  println(s"${britishPM.street1} ${britishPM.street2}")
